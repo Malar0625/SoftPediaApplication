c1dd8af24f784f3233781c8ca29f7f5ceb4a28c3
Admin <Admin@DESKTOP-10NDQ5O>
2020-03-25 09:23:59 +0530

    Extenreport_new

--- SoftPedia_Application/reports/cucumber-html-report/report.js
+++ SoftPedia_Application/reports/cucumber-html-report/report.js
@@ -14,9 +14,9 @@ formatter.feature({
 });
 formatter.scenario({
   "line": 5,
-  "name": "",
-  "description": "Register into softpedia web application",
-  "id": "softpedia-application;",
+  "name": "Register into softpedia web application",
+  "description": "",
+  "id": "softpedia-application;register-into-softpedia-web-application",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
@@ -27,16 +27,325 @@ formatter.scenario({
   ]
 });
 formatter.step({
-  "line": 7,
+  "line": 6,
   "name": "The user launch and register the application with valid and invalid details",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 8,
+  "line": 7,
   "name": "close the browser",
   "keyword": "And "
 });
 formatter.match({
   "location": "Definition_Register.the_user_launch_and_register_the_application_with_valid_and_invalid_details()"
+});
+formatter.result({
+  "duration": 82585991600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Register.close_the_browser()"
+});
+formatter.result({
+  "duration": 3762619800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Login into softpedia web applications",
+  "description": "",
+  "id": "softpedia-application;login-into-softpedia-web-applications",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@tc_02_Login"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "The user launch the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "clicking on the user icon and enter username and password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "clicking on the login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Definition_Login.the_user_launch_the_application()"
+});
+formatter.result({
+  "duration": 13002105800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Login.clicking_on_the_user_icon_and_enter_username_and_password()"
+});
+formatter.result({
+  "duration": 1588594100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Login.clicking_on_the_login_button()"
+});
+formatter.result({
+  "duration": 4038697000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "Search for application",
+  "description": "",
+  "id": "softpedia-application;search-for-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@tc_03_Search"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "The user launch the softpedia application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "The User enter the word for search",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "Click go button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "Download the application in the list",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 21,
+  "name": "Check file is exist or not",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Definition_Search.the_user_launch_the_softpedia_application()"
+});
+formatter.result({
+  "duration": 7661955300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Search.the_User_enter_the_word_for_search()"
+});
+formatter.result({
+  "duration": 190483200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Search.click_go_button()"
+});
+formatter.result({
+  "duration": 4747275700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Search.download_the_application_in_the_list()"
+});
+formatter.result({
+  "duration": 12713155100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Search.Check_file_is_exist_or_not()"
+});
+formatter.result({
+  "duration": 3388764500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 24,
+  "name": "Send feedback through contact us",
+  "description": "",
+  "id": "softpedia-application;send-feedback-through-contact-us",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 23,
+      "name": "@tc_04_Contactus"
+    }
+  ]
+});
+formatter.step({
+  "line": 25,
+  "name": "The user launch the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "click on i named icon and contact us",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "click radio button from the list",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 28,
+  "name": "The user enter a message and mail id",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 29,
+  "name": "clicking on send button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Definition_ContactUs.the_user_launch_the_browser()"
+});
+formatter.result({
+  "duration": 13471264600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_ContactUs.click_on_i_named_icon_and_contact_us()"
+});
+formatter.result({
+  "duration": 2587421500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_ContactUs.click_radio_button_from_the_list()"
+});
+formatter.result({
+  "duration": 146747400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_ContactUs.the_user_enter_a_message_and_mail_id()"
+});
+formatter.result({
+  "duration": 456000200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_ContactUs.clicking_on_send_button()"
+});
+formatter.result({
+  "duration": 4346939200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "Softpedia News Calendar",
+  "description": "",
+  "id": "softpedia-application;softpedia-news-calendar",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 31,
+      "name": "@tc_05_Calender"
+    }
+  ]
+});
+formatter.step({
+  "line": 34,
+  "name": "Launch the browser for calendar",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 35,
+  "name": "Select month and year",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 36,
+  "name": "Select the date",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 37,
+  "name": "Scroll and click any news",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Definition_Calendar.launch_the_browser_for_calendar()"
+});
+formatter.result({
+  "duration": 7488074400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Calendar.select_month_and_year()"
+});
+formatter.result({
+  "duration": 2018473700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Calendar.select_the_date()"
+});
+formatter.result({
+  "duration": 4517000100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_Calendar.scroll_and_click_any_news()"
+});
+formatter.result({
+  "duration": 5590900600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 40,
+  "name": "Navigation to the next page",
+  "description": "",
+  "id": "softpedia-application;navigation-to-the-next-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 39,
+      "name": "@tc_6_NextPage"
+    }
+  ]
+});
+formatter.step({
+  "line": 42,
+  "name": "The user Launch the application for nextpage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 43,
+  "name": "Scroll Down and click next page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Definition_NextPage.the_user_Launch_the_application_for_nextpage()"
+});
+formatter.result({
+  "duration": 13308712100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Definition_NextPage.scroll_Down_and_click_next_page()"
+});
+formatter.result({
+  "duration": 9949485000,
+  "status": "passed"
 });
 });
--- SoftPedia_Application/src/main/resources/feature/TestScenario.feature
+++ SoftPedia_Application/src/main/resources/feature/TestScenario.feature
@@ -2,21 +2,18 @@
 Feature: SoftPedia Application
 
 @tc_01_Register
-Scenario:
-	Register into softpedia web application
+Scenario: Register into softpedia web application
 	Given  The user launch and register the application with valid and invalid details
 	And close the browser
 
 @tc_02_Login
-Scenario:
-	Login into softpedia web applications
+Scenario: Login into softpedia web applications
 	Given The user launch the application
 	Then clicking on the user icon and enter username and password
 	And clicking on the login button
 
 @tc_03_Search
-Scenario:
-	Search for application
+Scenario: Search for application
 	Given The user launch the softpedia application
 	Then The User enter the word for search
 	Then Click go button
@@ -24,8 +21,7 @@ Scenario:
 	And Check file is exist or not
 	
 @tc_04_Contactus
-Scenario:
-	Send feedback through contact us
+Scenario: Send feedback through contact us
 	Given The user launch the browser
 	Then click on i named icon and contact us
 	Then click radio button from the list
@@ -33,8 +29,7 @@ Scenario:
 	And clicking on send button
 
 @tc_05_Calender	
-Scenario:
-	Softpedia News Calendar
+Scenario: Softpedia News Calendar
 	
 	Given Launch the browser for calendar
 	Then Select month and year
@@ -42,8 +37,7 @@ Scenario:
 	And Scroll and click any news 
 	
 @tc_6_NextPage
-Scenario:
-	Navigation to the next page
+Scenario: Navigation to the next page
 	
 	Given The user Launch the application for nextpage
 	Then Scroll Down and click next page
