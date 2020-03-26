00b9498fef8efc1b30ff537091e5bf6229e0bf40
Malar0625 <61264839+Malar0625@users.noreply.github.com>
2020-03-26 13:24:14 +0530

    commit 2

--- SoftPedia_Application/src/test/java/com/Pages/Page_Login.java
+++ SoftPedia_Application/src/test/java/com/Pages/Page_Login.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Page_Login 
 {
