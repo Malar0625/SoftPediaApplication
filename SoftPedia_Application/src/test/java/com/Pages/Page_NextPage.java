5550a934937648522e0538fcad091a46a2f46cd9
Malar0625 <61264839+Malar0625@users.noreply.github.com>
2020-03-26 13:25:28 +0530

    commit 2

--- SoftPedia_Application/src/test/java/com/Pages/Page_NextPage.java
+++ SoftPedia_Application/src/test/java/com/Pages/Page_NextPage.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Page_NextPage 
 {
@@ -18,13 +20,32 @@ public class Page_NextPage
 	By nextpage=By.xpath("/html/body/div/div[2]/div[10]/div[9]/div[22]/div[8]/div/a[11]");
 	By previouspage=By.xpath("//*[@id=\"sjmp\"]/div[9]/div[22]/div[8]/div/a[1]");
 	// code for launch browser
-	public void LaunchBrowser()
+	public void browserLaunch(String browser,String url)
 	{
-		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\Driver\\chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.get("https://linux.softpedia.com/");
+		try {
+					//To launch firefox Browser
+			if (browser.equalsIgnoreCase("firefox")) {
+				System.setProperty("webdriver.gecko.driver","src//test//resources//Driver//geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+					// To launch chrome Browser
+			else if (browser.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver","src//test//resources//Driver//chromedriver.exe");
+				driver=new ChromeDriver();
+			}
+					// To launch InternetExplorer
+			else if (browser.equalsIgnoreCase("Internet Explore")) {
+					System.setProperty("webdriver.ie.driver","src//test//resources//Driver//IEDriverServer.exe");
+				driver=new InternetExplorerDriver();  
+			}
+					//to maximize the window
+			driver.manage().window().maximize(); 
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.get(url);
+		
+		} catch (WebDriverException e) {
+			System.out.println("Browser could not be launched");
+		}
 	}
 	//code for scrolling nextpage to previous page
 	public void Scroll_clicknext()
